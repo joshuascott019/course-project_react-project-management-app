df4dbcde7a86e743034cd72a79ed4f8689aec7e1
Joshua Scott <joshua.ds.s019@gmail.com>
2024-05-17 13:46:01 -0700

    feat: 'creat new project' and 'add project' buttons now function by opening the input field for a new project

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import noProjectImage from '../assets/no-projects.png';
 import Button from './Button';
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -15,7 +15,7 @@ export default function NoProjectSelected() {
       </h2>
       <p className="text-stone-400 mb-4">Select or start a new project</p>
       <p className="mt-8">
-        <Button>Creat New Project</Button>
+        <Button onClick={onStartAddProject}>Creat New Project</Button>
       </p>
     </div>
   );
