5d5eccebd2848563d96326171ed71c67d9e635a7
Joshua Scott <joshua.ds.s019@gmail.com>
2024-05-17 13:31:47 -0700

    feat: add No Project Selected component refector: add reusable Button component

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import noProjectImage from '../assets/no-projects.png';
+import Button from './Button';
+
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImage}
+        alt="An empty task list"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold- text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">Select or start a new project</p>
+      <p className="mt-8">
+        <Button>Creat New Project</Button>
+      </p>
+    </div>
+  );
+}
