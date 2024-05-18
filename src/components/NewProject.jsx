9f8c1be9264df7ed0d2090203bb0235d405f17d3
Joshua Scott <joshua.ds.s019@gmail.com>
2024-05-18 14:23:00 -0700

    feat: add functionality to cancel button for create new project window style: add styling to error/invalid input modal popup

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import { React, useRef } from 'react';
 import Input from './Input';
 import Modal from './Modal';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
 
   const title = useRef();
@@ -34,13 +34,20 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Okay">
-        <h2>Invalid Input</h2>
-        <p>Please provide valid entries for each field.</p>
+        <h2 className="text-xl font-bold- text-stone-700 my-4">
+          Invalid Input
+        </h2>
+        <p className="text-stone-600 mb-4">
+          Please provide valid entries for each field.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16 ">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
               Cancel
             </button>
           </li>
